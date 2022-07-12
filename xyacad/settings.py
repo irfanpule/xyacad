8adb9bef3f045af8ed1b3900c37d6dbf0b59211f
irfanpule <irfan.pule2@gmail.com>
2022-07-12 15:29:53 +0700

    add app akademik, sekolah, pegawai, ui

--- xyacad/settings.py
+++ xyacad/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'akademik',
+    'sekolah',
+    'pegawai',
+    'ui'
 ]
 
 MIDDLEWARE = [
