2c4ed84564f9a9f17b0370ae02b900706585552c
irfanpule <irfan.pule2@gmail.com>
2022-07-14 10:44:43 +0700

    add model and admin module akademik

--- akademik/admin.py
+++ akademik/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from akademik.models import (
+    TahunAkademik, Tingkat, Kurikulum, KelompokMalep, MataPelajaran
+)
 
-# Register your models here.
+
+class AdminTahunAkademik(admin.ModelAdmin):
+    pass
+
+
+class AdminTingkat(admin.ModelAdmin):
+    pass
+
+
+class AdminKurikulum(admin.ModelAdmin):
+    pass
+
+
+class AdminKelompokMapel(admin.ModelAdmin):
+    pass
+
+
+class AdminMataPelajaran(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TahunAkademik, AdminTahunAkademik)
+admin.site.register(Tingkat, AdminTingkat)
+admin.site.register(Kurikulum, AdminKurikulum)
+admin.site.register(KelompokMalep, AdminKelompokMapel)
+admin.site.register(MataPelajaran, AdminMataPelajaran)
--- akademik/models.py
+++ akademik/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from core.models import BaseModel
+from sekolah.models import Sekolah, Jurusan
+from pegawai.models import Pegawai
 
-# Create your models here.
+
+class TahunAkademik(BaseModel):
+    nama = models.CharField('Nama Tahun Akademik', max_length=150)
+    kode = models.CharField('Kode Tahun Akademik', max_length=150)
+    ket = models.CharField('Keterangan', max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nama} - {self.id}"
+
+
+class Tingkat(BaseModel):
+    nama = models.CharField('Nama Tingkat', max_length=150)
+    kode = models.CharField('Kode Tingkat', max_length=150)
+    ket = models.CharField('Keterangan', max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nama} - {self.id}"
+
+
+class Kurikulum(BaseModel):
+    nama = models.CharField('Nama Kurikulum', max_length=150)
+    ket = models.CharField('Keterangan', max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nama} - {self.id}"
+
+
+class KelompokMalep(BaseModel):
+    nama = models.CharField('Nama Kelompok', max_length=150)
+    jenis = models.CharField('Jenis', max_length=150)
+    sekolah = models.ForeignKey(Sekolah, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nama} - {self.id}"
+
+
+class MataPelajaran(BaseModel):
+    nama = models.CharField('Nama Mata Pelajaran', max_length=150)
+    kode = models.CharField('Kode Mata Pelajaran', max_length=150)
+    jml_jam = models.PositiveSmallIntegerField('Jumlah Jam')
+    tingkat = models.ForeignKey(Tingkat, on_delete=models.CASCADE)
+    kel_mapel = models.ForeignKey(KelompokMalep, on_delete=models.SET_NULL, blank=True, null=True)
+    jurusan = models.ForeignKey(Jurusan, on_delete=models.SET_NULL, blank=True, null=True)
+    guru = models.ForeignKey(Pegawai, on_delete=models.SET_NULL, blank=True, null=True)
+    kurikulum = models.ForeignKey(Kurikulum, on_delete=models.SET_NULL, blank=True, null=True)
+    umum = models.CharField('Kompetensi Umum', max_length=200)
+    khusu = models.CharField('Kompetensi Khusus', max_length=200)
+
+    def __str__(self):
+        return f"{self.nama} - {self.id}"
