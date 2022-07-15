12c9a0ae7a89757600e3f7b0ad1cdfd5353bd971
irfanpule <irfan.pule2@gmail.com>
2022-07-15 14:25:49 +0700

    base auth login logout template

--- xyacad/settings.py
+++ xyacad/settings.py
@@ -130,6 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
