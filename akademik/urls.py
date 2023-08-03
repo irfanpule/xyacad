46c61c21c1af66f554e573ccad698c294430dd9e
irfanpule <irfan.pule2@gmail.com>
2023-08-03 10:56:20 +0700

    change url name tahunakademik and kurikulum

--- akademik/urls.py
+++ akademik/urls.py
@@ -3,18 +3,18 @@ from akademik import views
 
 app_name = 'akademik'
 urlpatterns = [
-    path('list/tahun/', views.TahunAkademikListView.as_view(), name='list_tahun'),
-    path('create/tahun/', views.TahunAkademikCreateView.as_view(), name='create_tahun'),
-    path('edit/tahun/<uuid:id>/', views.TahunAkademikUpdateView.as_view(), name='edit_tahun'),
-    path('delete/tahun/<uuid:id>/', views.TahunAkademikDeleteView.as_view(), name='delete_tahun'),
+    path('tahun/list/', views.TahunAkademikListView.as_view(), name='tahunakademik_list'),
+    path('tahun/create/', views.TahunAkademikCreateView.as_view(), name='tahunakademik_create'),
+    path('tahun/edit/<uuid:id>/', views.TahunAkademikUpdateView.as_view(), name='tahunakademik_update'),
+    path('tahun/delete/<uuid:id>/', views.TahunAkademikDeleteView.as_view(), name='tahunakademik_delete'),
 
-    path('list/kurikulum/', views.KurikulumListView.as_view(), name='list_kurikulum'),
-    path('create/kurikulum/', views.KurikulumCreateView.as_view(), name='create_kurikulum'),
-    path('edit/kurikulum/<uuid:id>/', views.KurikulumUpdateView.as_view(), name='edit_kurikulum'),
-    path('delete/kurikulum/<uuid:id>/', views.KurikulumDeleteView.as_view(), name='delete_kurikulum'),
+    path('kurikulum/list/', views.KurikulumListView.as_view(), name='kurikulum_list'),
+    path('kurikulum/create/', views.KurikulumCreateView.as_view(), name='kurikulum_create'),
+    path('kurikulum/edit/<uuid:id>/', views.KurikulumUpdateView.as_view(), name='kurikulum_update'),
+    path('kurikulum/delete/<uuid:id>/', views.KurikulumDeleteView.as_view(), name='kurikulum_delete'),
 
-    path('list/kolompokmapel/', views.KelompokMapelListView.as_view(), name='list_kelompokmapel'),
-    path('create/kolompokmapel/', views.KelompokMapelCreateView.as_view(), name='create_kelompokmapel'),
-    path('edit/kolompokmapel/<uuid:id>/', views.KelompokMapelUpdateView.as_view(), name='edit_kelompokmapel'),
-    path('delete/kolompokmapel/<uuid:id>/', views.KelompokMapelDeleteView.as_view(), name='delete_kelompokmapel'),
+    path('kolompokmapel/list/', views.KelompokMapelListView.as_view(), name='list_kelompokmapel'),
+    path('kolompokmapel/create/', views.KelompokMapelCreateView.as_view(), name='create_kelompokmapel'),
+    path('kolompokmapel/edit/<uuid:id>/', views.KelompokMapelUpdateView.as_view(), name='edit_kelompokmapel'),
+    path('kolompokmapel/delete/<uuid:id>/', views.KelompokMapelDeleteView.as_view(), name='delete_kelompokmapel'),
 ]
