d682a093c4c1ae853634274c77d29db075249e45
irfanpule <irfan.pule2@gmail.com>
2023-08-24 14:05:31 +0700

    addons list menu

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,12 +6,15 @@ from addons import get_addons_list
 
 @login_required
 def home(request):
-    sweetify.toast(
-        request, 'Welcome to home page', icon='info', persistent="Bye toast!")
+    return render(request, 'dashboard/home.html')
+
+
+@login_required
+def addons_app_list(request):
     context = {
         'addons_list': get_addons_list()
     }
-    return render(request, 'dashboard/home.html', context)
+    return render(request, 'dashboard/addosn_app_list.html', context)
 
 
 def form_general(request):
